57872e9ef9ddf81c4bfba2ca2bcaf1d522ce6aa4
Matt Twardowski <mttwardowski@gmail.com>
2017-05-22 15:25:44 -0500

    Removed Rails Routes and mounted ResourcesApi

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,4 @@
 MailFunnelServer::Application.routes.draw do
-  resources :captured_hooks
-  resources :email_templates
-  resources :email_lists
-  resources :subscribers
-  # http://localhost:3001/API/
-	mount API => '/'
-
-  # resources :links
-  # resources :nodes
-  # resources :triggers
-  # resources :funnels
-  # resources :campaign_product_leads
-
-
-
-	# http://localhost:3001/RESOURCE
-
-	# resources :email_lists do
-	# 	resources :emails
-	# end
-
-	# resources :emails
-	# resources :hooks
-
-	# resources :apps do
-	# 	resources :email_lists do
-	# 		resources :email
-	# 	end
-	# end
-
+	mount ResourceApi => '/'
 
 end
